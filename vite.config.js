337a889fb17d49074100ac8527069affa0f623e2
Obada <abodbdran999@gmail.com>
2024-04-01 09:16:07 -0700

    third commit

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
+const babelPlugins = [];
+
+if (process.env.MIGHTYMELD) {
+  babelPlugins.push('@mightymeld/runtime/babel-plugin-mightymeld');
+}
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ react({
+    babel: {
+      plugins: babelPlugins
+    }
+  })],
 })
